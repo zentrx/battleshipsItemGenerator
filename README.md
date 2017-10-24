74b23501b0f6a5037b9f34c429c7b1de96add136
zentrix <zentrix718@gmail.com>
2017-10-23 20:28:54 -0700

    added webpack
    most of the way through the setup from this mr. farmer fellow. 
    
    https://www.andrewhfarmer.com/build-your-own-starter/#7-react

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # battleshipsItemGenerator
+---
 Generate items for Dota 2 Battleships Mod
+
+#setup
+---
+```
+npm install
+```
+
+
+#compile
+---
+```
+npm run compile
+```
--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 const express = require('express');
+const webpack = require('webpack');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const webpackConfig = require('./webpack.config.js');
 const app = express();
 const path = require('path');
+const compiler = webpack(webpackConfig);
+
+app.use(express.static(path.join(__dirname, 'www')))
+
+app.use(webpackDevMiddleware(compiler, {
+	hot: true,
+	filename: 'bundle.js', 
+	publicPath: '/',
+	stats: {
+		colors: true,
+	},
+	historyApiFallback: true,
+}));
 
 app.get('/', function (req, res) {
 	res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(3000, function() {
-
+const server = app.listen(3000, function() {
+	const host = server.address().address;
+	const port = server.address().port;
+	console.log('App listening at http:\\%s:%s', host, port)
 });
